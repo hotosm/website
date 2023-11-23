76224fa8a71b1ccf62b6cd1c44c74f5d0bb6a338
Kat Olaguer <katherine.olaguer@gmail.com>
2023-11-22 19:48:15 -0800

    Added User factory and tests

---
+++ users/factory.py
@@ -0,0 +1,16 @@
+import factory
+from pytest_factoryboy import register
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    username = factory.Sequence(lambda n: "user%d" % n)
+    first_name = factory.Sequence(lambda n: "first_name%d" % n)
+    last_name = factory.Sequence(lambda n: "last_name%d" % n)
+    email = factory.Sequence(lambda n: "email%d@hotosm.com" % n)
+    password = factory.PostGenerationMethodCall("set_password", "password")
+
+    class Meta:
+        model = "users.User"
+
+
+register(UserFactory)

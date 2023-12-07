6d775c64b64562bc70b634eef5865a78a9e695b7
Kat Olaguer <katherine.olaguer@gmail.com>
2023-12-07 00:27:31 -0800

    Extended theme with custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   purge: ['./**/*.html', './**/*.js',],
   content: ["*/templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "hot-red": "#D73F3F",
+        "hot-navy": "#20365B",
+        "hot-dark-grey": "#68707F",
+        "hot-slate-grey": "#929D83",
+        "hot-light-grey": "#E1E0E0",
+        "hot-off-white": "#F0EFEF",
+        "hot-white": "#FFFFFF",
+        "hot-black": "#000000",
+      }
+    },
   },
   variants: {
     extend: {},

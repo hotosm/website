4bf6fdac99cc2483c1b91f134fe694d59d39cbba
Kat Olaguer <katherine.olaguer@gmail.com>
2023-12-07 01:26:36 -0800

    Added Tailwind custom spacing

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./**/*.html', './**/*.js',],
-  content: ["*/templates/**/*.html"],
   theme: {
+    spacing: {
+      '1': '10px',
+      '1.2': '12px',
+      '1.5': '15px',
+      '2': '20px',
+      '2.6': '26px',
+      '3': '30px',
+      '4': '40px',
+      '6': '60px',
+      '6.6': '66px',
+      '8': '80px',
+      '10': '100px',
+      '12': '120px',
+    },
     extend: {
       colors: {
         "hot-red": "#D73F3F",

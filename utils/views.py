537a7510288b46ecdc41a57c33f5a794cc6c77bb
Kat Olaguer <katherine.olaguer@gmail.com>
2024-03-10 04:53:38 -0700

    Rendered language dropdown, not yet submittable

--- utils/views.py
+++ utils/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.utils import translation
 
-# Create your views here.
+
+def set_language(request):
+    language_code = request.GET.get("language_code")
+    translation.activate(language_code)
+    request.session[translation.LANGUAGE_SESSION_KEY] = language_code
+    return redirect(request.META.get("HTTP_REFERER"))

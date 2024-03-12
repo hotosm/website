5089580daa9665eb3c0ea02713f558ac3211644e
Kat Olaguer <katherine.olaguer@gmail.com>
2024-03-12 14:59:20 -0700

    Removed unneeded comments

--- utils/urls.py
+++
@@ -1,7 +0,0 @@
-# from django.urls import include, path
-
-# app_name = "utils"
-# urlpatterns = [
-#     # ... your other urls
-#     path("i18n/", include("django.conf.urls.i18n")),
-# ]
--- utils/views.py
+++
@@ -1,9 +0,0 @@
-# from django.shortcuts import redirect
-# from django.utils import translation
-
-
-# def set_language(request):
-#     language_code = request.POST.get("language_code")
-#     translation.activate(language_code)
-#     request.session["django_language"] = language_code
-#     return redirect(request.META.get("HTTP_REFERER"))

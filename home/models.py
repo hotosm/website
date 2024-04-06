8569ff7a229654eddb4e367497aa15803d980139
Kat Olaguer <katherine.olaguer@gmail.com>
2024-04-05 17:45:59 -0700

    Added editable CTA to homepage

--- home/models.py
+++ home/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    templates = "home/home_page.html"
+
+    landing_page_cta = models.CharField(max_length=100, blank=False, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("landing_page_cta"),
+    ]

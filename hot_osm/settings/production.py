77e48ab415b4c86713b795b0bef325e9878d4e67
luminaryFlowers <71102109+luminaryFlowers@users.noreply.github.com>
2024-06-18 09:39:10 -0700

    fly.io work mostly done => still need aws login in order to get media functioning => also did minor changes to some styling things

--- hot_osm/settings/production.py
+++ hot_osm/settings/production.py
@@ -1,6 +1,15 @@
 from .base import *
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["hotosm-staging-new.fly.dev", "127.0.0.1", "localhost"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+INSTALLED_APPS = INSTALLED_APPS + ["django_browser_reload"]  # noqa: F405
+MIDDLEWARE = MIDDLEWARE + ["django_browser_reload.middleware.BrowserReloadMiddleware"]  # noqa: F405
 
 try:
     from .local import *

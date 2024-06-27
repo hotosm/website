f19db535a73e40eee665950c0a2923dfc6703579
luminaryFlowers <71102109+luminaryFlowers@users.noreply.github.com>
2024-06-27 09:36:43 -0700

    news page ready to go => search by keyword, category select, sorting by date/title, and search by tag

--- app/news/models.py
+++ app/news/models.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.db import models
+from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.contrib.taggit import ClusterTaggableManager
@@ -10,9 +12,56 @@ from wagtail.fields import RichTextField, StreamField
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
 from wagtail.blocks import CharBlock, StreamBlock, StructBlock, URLBlock, RichTextBlock, PageChooserBlock
 from wagtail.snippets.models import register_snippet
+from wagtail.search import index
 
 
 class NewsOwnerPage(Page):
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+
+        keyword = request.GET.get('keyword', '')
+        news_list = IndividualNewsPage.objects.live().filter(locale=context['page'].locale)
+
+        categories = NewsCategory.objects.all()
+        tags = [x[4:] for x in request.GET.keys() if x.startswith("tag.")]
+        query = Q()
+        for category in categories:
+            if request.GET.get(str(category), ''):
+                query = query | Q(categories=category)
+        for tag in tags:
+            query = query | Q(tags__name=tag)
+        news_list = news_list.filter(query).distinct()
+
+        match request.GET.get('sort', ''):
+            case 'sort.new':
+                news_list = news_list.order_by('date')
+            case 'sort.old':
+                news_list = news_list.order_by('-date')
+            case 'sort.titlea':
+                news_list = news_list.order_by('title')
+            case 'sort.titlez':
+                news_list = news_list.order_by('-title')
+            case _:
+                news_list = news_list.order_by('date')
+
+        if keyword:
+            news_list = news_list.search(keyword)
+
+        page = request.GET.get('page', 1)
+        paginator = Paginator(news_list, 6)  # if you want more/less items per page (i.e., per load), change the number here to something else
+        try:
+            news = paginator.page(page)
+        except PageNotAnInteger:
+            news = paginator.page(1)
+        except EmptyPage:
+            news = paginator.page(paginator.num_pages)
+        
+        context['news'] = news
+        context['news_paginator'] = paginator
+        context['current_page'] = int(page)
+        context['categories'] = categories
+        return context
+    
     max_count = 1
 
     authors_posted_by_text = models.CharField(default="Posted by", help_text="The text which appears prior to the authors names; with 'posted by', the text displays as 'posted by [author]'.")
@@ -25,20 +74,45 @@ class NewsOwnerPage(Page):
     categories_title = models.CharField(default="Categories")
     tags_title = models.CharField(default="Tags")
 
+    keyword_search_hint = models.CharField(default="Search by keyword")
+    category_select = models.CharField(default="Select Categories")
+    sort_by_new = models.CharField(default="Sort by New")
+    sort_by_old = models.CharField(default="Sort by Old")
+    sort_by_titlea = models.CharField(default="Sort by Title Alphabetical")
+    sort_by_titlez = models.CharField(default="Sort by Title Reverse Alphabetical")
+    enter_tag_hint = models.CharField(default="Enter tag")
+    search_button_text = models.CharField(default="Search")
+    remove_filters_text = models.CharField(default="Remove All Filters")
+    results_text = models.CharField(default="Results")
+
     content_panels = Page.content_panels + [
         MultiFieldPanel([
-            FieldPanel("authors_posted_by_text"),
-            FieldPanel("authors_posted_on_text"),
-        ], heading="Info"),
+            FieldPanel('keyword_search_hint'),
+            FieldPanel('category_select'),
+            FieldPanel('sort_by_new'),
+            FieldPanel('sort_by_old'),
+            FieldPanel('sort_by_titlea'),
+            FieldPanel('sort_by_titlez'),
+            FieldPanel('enter_tag_hint'),
+            FieldPanel('search_button_text'),
+            FieldPanel('remove_filters_text'),
+            FieldPanel('results_text'),
+        ], heading="News Search Page"),
         MultiFieldPanel([
-            FieldPanel('related_projects_title'),
-            FieldPanel('related_news_title'),
-            FieldPanel('view_all_news_text'),
-            FieldPanel('view_all_news_url'),
-            FieldPanel('news_read_more_text'),
-            FieldPanel('categories_title'),
-            FieldPanel('tags_title'),
-        ], heading="Sidebar"),
+            MultiFieldPanel([
+                FieldPanel("authors_posted_by_text"),
+                FieldPanel("authors_posted_on_text"),
+            ], heading="Info"),
+            MultiFieldPanel([
+                FieldPanel('related_projects_title'),
+                FieldPanel('related_news_title'),
+                FieldPanel('view_all_news_text'),
+                FieldPanel('view_all_news_url'),
+                FieldPanel('news_read_more_text'),
+                FieldPanel('categories_title'),
+                FieldPanel('tags_title'),
+            ], heading="Sidebar"),
+        ], heading="Individual Project Page"),
     ]
 
 
@@ -102,6 +176,13 @@ class IndividualNewsPage(Page):
     categories = ParentalManyToManyField('news.NewsCategory', blank=True)
 
     tags = ClusterTaggableManager(through=NewsTag, blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('title'),
+        index.SearchField('intro'),
+        index.FilterField('newscategory_id'),  # the console warns you about this but if you don't have this then category search doesn't work
+        index.FilterField('name'),
+    ]
 
     content_panels = Page.content_panels + [
         MultiFieldPanel([

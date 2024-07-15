1800ebe201399fe3dfde304e839e94490a7c69f9
luminaryFlowers <71102109+luminaryFlowers@users.noreply.github.com>
2024-07-15 12:05:35 -0700

    individual volunteer opportunity page

---
+++ app/volunteer_opportunities/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VolunteerOpportunitiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app.volunteer_opportunities'
